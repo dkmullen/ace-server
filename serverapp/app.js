f4426459b966a8b85904d35d135b3b66eb680ef1
Dennis <dkmullen@gmail.com>
2019-11-17 16:04:49 -0500

    Add email functions

--- serverapp/app.js
+++ serverapp/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
 
 const app = express();
 
@@ -32,21 +33,60 @@ app.use((req, res, next) => {
 
 app.post("/api/posts", (req, res, next) => {
   const post = new Post({
-      name: req.body.name,
-      email: req.body.email,
-      phone: req.body.phone,
-      age: req.body.age,
-      grade: req.body.grade,
-      school: req.body.school,
-      rising: req.body.rising,
-      individualVocal: req.body.individualVocal,
-      individualInstrumental: req.body.individualInstrumental,
-      group: req.body.group
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    age: req.body.age,
+    grade: req.body.grade,
+    school: req.body.school,
+    rising: req.body.rising,
+    individualVocal: req.body.individualVocal,
+    individualInstrumental: req.body.individualInstrumental,
+    group: req.body.group
   })
   post.save();
   res.status(201).json({
     message: 'Post added successfully'
   });
+  main(post).catch(console.error);
 });
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main(post) { console.log('here', JSON.stringify(post))
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_APP_PW
+  }
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Dennis Mullen - ACE Awards Form" <dkmullen@gmail.com>', // sender address
+    to: 'dkmullen@gmail.com', // list of receivers
+    subject: 'A new contestant for the ACE Singing Awards!', // Subject line
+    text: 'Whatevs', // plain text body
+    html: `<b>ACE Singing Awards Sign-up</b> (from aceknox.com)<br />
+            <p>A new contestant has signed up for the ACE Singing Awards:</p>
+            Name: ${post.name}<br />
+            Age: ${post.age}<br />
+            Email: ${post.email}<br />
+            Phone: ${post.phone}<br />
+            Grade: ${post.grade}<br />
+            School: ${post.school}<br />
+            Rising Star?: ${post.rising}<br />
+            Individual Vocal?: ${post.individualVocal}<br />
+            Individual Instrumental?: ${post.individualInstrumental}<br />
+            Group?: ${post.group}<br />`
+  });
+
+  console.log('Message sent: %s', info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+}
 
 module.exports = app;
