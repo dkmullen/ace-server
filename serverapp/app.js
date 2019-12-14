ae2ac020d0234ed025e4384488e3d695afa66881
Dennis <dkmullen@gmail.com>
2019-12-14 08:32:04 -0500

    change port

--- serverapp/app.js
+++ serverapp/app.js
@@ -104,8 +104,8 @@ app.post("/api/acting-team-posts", (req, res, next) => {
 // create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true, // true for 465, false for other ports
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_APP_PW
