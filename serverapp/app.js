6c37a5cf159a354d5f444454100c9bfb51a3639c
Dennis Mullen <dkmullen@gmail.com>
2021-04-22 06:37:51 -0400

    Add cors

--- serverapp/app.js
+++ serverapp/app.js
@@ -7,6 +7,27 @@ const app = express();
 
 const postsRoutes = require("./routes/posts");
 
+// add this code
+const whitelist = ['http://localhost:3000']; // list of allow domain
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) {
+            return callback(null, true);
+        }
+
+        if (whitelist.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}
+
+// end 
+app.use(cors(corsOptions));
+
 mongoose.connect(process.env.ATLAS_CREDS, 
     { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
